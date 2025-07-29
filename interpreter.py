9716d0766ee8dc96bbb56709d20b375dc5bcd3a5
astra-the-boop <astra.the.boop@icloud.com>
2025-07-29 12:35:56 +0700

    fixed the indexing error

--- interpreter.py
+++ interpreter.py
@@ -1,13 +1,17 @@
 from parser import parse
 
 def interpreter(filename):
-    print(parse(filename))
     data = parse(filename)
     for i in range(len(data)):
-        current = data[i]
-        next = data[i + 1]
-        if current["type"] == "text":
-            if next["type"] == "note" and next["pitch"].startswith("C"):
-                print(current["text"])
+        try:
+            current = data[i]
+            next = data[i + 1]
+            if current["type"] == "text":
+                if next["type"] == "note" and next["pitch"].startswith("C"):
+                    print(current["text"])
+        except IndexError:
+            pass
+
+
 
 interpreter("Untitled score.musicxml")
