87b6e7496ec23501ea87a63400fb2589475289ac
astra-the-boop <astra.the.boop@icloud.com>
2025-07-29 16:27:20 +0700

    i'm so fucking confused ugh

--- parser.py
+++ parser.py
@@ -1,31 +1,31 @@
-from music21 import converter, note, meter, tempo, stream, expressions, bar
+from music21 import converter, note, expressions, bar
 
 def parse(filename):
     score = converter.parse(filename)
-    flatScore = score.flatten()
     output = []
 
     for part in score.parts:
         for measure in part.getElementsByClass("Measure"):
-            if measure.leftBarline and isinstance(measure.leftBarline, bar.Repeat):
-                if measure.leftBarline.direction == "start":
-                    output.append({"type": "repeatStart"})
+            barlineLeft = measure.leftBarline
+            barlineRight = measure.rightBarline
 
-                if measure.rightBarline and isinstance(measure.rightBarline, bar.Repeat):
-                    if measure.rightBarline.direction == "end":
-                        output.append({"type": "repeatEnd"})
+            if isinstance(barlineLeft, bar.Repeat) and barlineLeft.direction == "start":
+                output.append({"type": "repeatStart"})
+
+            if isinstance(barlineRight, bar.Repeat) and barlineRight.direction == "end":
+                output.append({"type": "repeatEnd"})
 
             for element in measure:
                 if isinstance(element, note.Note):
                     pitch = element.nameWithOctave
-                    duration = element.duration.quarterLength
+                    duration = element.quarterLength
                     output.append({"type": "note", "pitch": pitch, "duration": duration})
                 elif isinstance(element, note.Rest):
-                    duration = element.quarterLength
-                    output.append({"type": "rest", "duration": duration})
+                    output.append({"type": "rest", "duration": element.quarterLength})
                 elif isinstance(element, expressions.TextExpression):
-                    output.append({"type": "text", "text": element.content.strip()})
+                    output.append({"type":"text", "text": element.content.strip()})
 
     return output
+
 
 print(parse("Untitled score.musicxml"))
