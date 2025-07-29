69beab12f24b85d981b2f3815615558ad9c97d16
astra-the-boop <astra.the.boop@icloud.com>
2025-07-29 14:00:12 +0700

    uah what now... (trying to fix parser)

--- interpreter.py
+++ interpreter.py
@@ -4,11 +4,27 @@ def interpreter(filename):
     data = parse(filename)
     for i in range(len(data)):
         try:
+            previous = data[i - 1]
             current = data[i]
             next = data[i + 1]
+
+            # PRINT
+
             if current["type"] == "text":
                 if next["type"] == "note" and next["pitch"].startswith("C"):
                     print(current["text"])
+
+            # VARIABLES
+
+        # DECLARING VARIABLES
+            if current["type"] == "repeatStart":
+                j = 0
+
+                while data[i + j]["type"] != "repeatEnd":
+                    if data[i + j]["type"] == "note" and data[i + j]["pitch"].startswith("D"):
+
+
+
         except IndexError:
             pass
 
--- parser.py
+++ parser.py
@@ -1,40 +1,30 @@
-from music21 import converter, note, meter, tempo, stream, expressions
+from music21 import converter, note, meter, tempo, stream, expressions, bar
 
 def parse(filename):
     score = converter.parse(filename)
     flatScore = score.flatten()
     output = []
 
-    for element in flatScore:
-        if isinstance(element, tempo.MetronomeMark):
-            output.append({
-                "type": "tempo",
-                "value": int(element.number),
-            })
-        elif isinstance(element, meter.TimeSignature):
-            output.append({
-                "type": "time",
-                "numerator": element.numerator,
-                "denominator": element.denominator,
-            })
-        elif isinstance(element, note.Note):
-            pitch = element.nameWithOctave
-            duration = element.quarterLength
-            output.append({"type": "note", "pitch": pitch, "duration": duration})
+    for part in score.parts:
+        for measure in part.getElementsByClass("Measure"):
+            if measure.leftBarline and isinstance(measure.leftBarline, bar.Repeat):
+                if measure.leftBarline.direction == "start":
+                    output.append({"type": "repeatStart"})
 
-        elif isinstance(element, note.Rest):
-            duration = element.quarterLength
-            output.append({
-                "type": "rest",
-                "duration": duration,
-            })
+                if measure.rightBarline and isinstance(measure.rightBarline, bar.Repeat):
+                    if measure.rightBarline.direction == "end":
+                        output.append({"type": "repeatEnd"})
 
-        elif isinstance(element, expressions.TextExpression):
-            output.append({
-                "type": "text",
-                "text": element.content.strip()
-            })
-
+                for element in measure:
+                    if isinstance(element, note.Note):
+                        pitch = element.nameWithOctave
+                        duration = element.duration.quarterLength
+                        output.append({"type":"note", "pitch":pitch, "duration":duration})
+                    elif isinstance(element, note.Rest):
+                        duration = element.quarterLength
+                        output.append({"type":"rest", "duration":duration})
+                    elif isinstance(element, expressions.TextExpression):
+                        output.append({"type":"text", "text":element.content.strip()})
 
     return output
 
