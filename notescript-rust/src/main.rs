f81052306cb5995002070d4edd4522a2197cb3bc
astra-the-boop <astra.the.boop@icloud.com>
2025-11-24 14:27:21 +0700

    i hate doing shit cross programming languages (kill me)

--- notescript-rust/src/main.rs
+++ notescript-rust/src/main.rs
@@ -2,5 +2,20 @@ use pyo3::prelude::*;
 use pyo3::types::{PyModule, PyTuple};
 use serde_json::Value;
 
+fn parser(path:&str) -> Value{
+    Python::with_gil(|py| {
+        let src = include_str!("parser.py");
+
+        let module = PyModule::from_code(py, src, "parser.py", "parser"
+        ).expect("Couldn't load parser.py");
+
+        let result = module.getattr("parse").unwrap()
+            .call1(PyTuple::new(py,&[path])).unwrap();
+
+        result.extract::<Value>().unwrap()
+    })
+
+}
+
 
 fn main() {}
