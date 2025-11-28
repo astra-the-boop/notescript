9da44dda23bbfd6826e0f2d6cc5053ab3506157f
astra-the-boop <astra.the.boop@icloud.com>
2025-11-28 14:08:08 +0700

    ADFSJNNJSDJFLWEKKKFKFFV

--- notescript-rust/src/main.rs
+++ notescript-rust/src/main.rs
@@ -1,6 +1,5 @@
 use pyo3::prelude::*;
-use std::ffi::CString;
-use pyo3::types::{PyModule, PyTuple};
+use pyo3::types::PyModule;
 use serde_json::Value;
 
 #[derive(Debug)]
@@ -16,44 +15,34 @@ fn parser(path: &str) -> Value {
     Python::with_gil(|py| {
         let src = include_str!("parser.py");
 
-        let code = CString::new(src).unwrap();
-        let filename = CString::new("parser.py").unwrap();
-        let module_name = CString::new("parser").unwrap();
-
-        let module = PyModule::from_code(py, &code, &filename, &module_name)
+        let module = PyModule::from_code(py, src, "parser.py", "parser")
             .expect("no load parser.py :(");
 
-        let results = module
-            .getattr("parse")
-            .unwrap()
-            .call1((path,))
-            .unwrap();
+        let raw_json: String = module
+            .getattr("parse").unwrap()
+            .call1((path,)).unwrap()
+            .extract().unwrap();
 
-        results.extract::<Value>().unwrap()
+        serde_json::from_str(&raw_json).unwrap()
     })
 }
 
-
 fn fuckMyLife(json: &Value) -> Vec<Event> {
     let mut out = Vec::new();
-    let arr = json.as_array().expect("parser shalt return a list >:c hmph");
+    let arr = json.as_array().expect("parser must return list");
 
-    for i in arr{
+    for i in arr {
         let obj = i.as_object().unwrap();
-        let eType = obj["type"].as_str().unwrap();
+        let e_ty = obj["type"].as_str().unwrap();
 
-        match eType {
-            "note" => {
-                out.push(Event::Note {
-                    pitch:obj["pitch"].as_str().unwrap().to_string(),
-                    duration: obj["duration"].as_f64().unwrap() as f32,
-                });
-            }
-            "text" => {
-                out.push(Event::Text(
-                    obj["text"].as_str().unwrap().to_string(),
-                ));
-            }
+        match e_ty {
+            "note" => out.push(Event::Note {
+                pitch: obj["pitch"].as_str().unwrap().to_string(),
+                duration: obj["duration"].as_f64().unwrap() as f32,
+            }),
+            "text" => out.push(Event::Text(
+                obj["text"].as_str().unwrap().to_string(),
+            )),
             "repeatStart" => out.push(Event::RepeatStart),
             "repeatEnd" => out.push(Event::RepeatEnd),
             other => panic!("unknown event: {}", other),
@@ -61,11 +50,16 @@ fn fuckMyLife(json: &Value) -> Vec<Event> {
     }
 
     out
-
 }
 
-
 fn main() {
+    pyo3::prepare_freethreaded_python();
+
+    let raw = parser("demo.musicxml");
+    let events = fuckMyLife(&raw);
+
+    println!("{:#?}", events);
+
     let raw = parser("demo.musicxml");
     let events = fuckMyLife(&raw);
 
